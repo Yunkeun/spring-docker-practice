d32fa5913026a521b8e14f70075863a33547da8d
Yunkeun <yunx2@naver.com>
2022-03-29 20:58:28 +0900

    Initial commit

---
+++ backend/src/main/java/com/yoonveloping/playwithdocker/PlaywithdockerApplication.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.playwithdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywithdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywithdockerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yoonveloping/playwithdocker/PlaywithdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.playwithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaywithdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
